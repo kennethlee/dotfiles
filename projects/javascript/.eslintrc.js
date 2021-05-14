282ca4a28dc34d5bc371b233da6569e202d23a11
Kenneth Lee <kennyxlee@gmail.com>
2021-05-13 23:24:02 -0700

    Move .eslintrc

--- projects/javascript/.eslintrc.js
+++ projects/javascript/.eslintrc.js
@@ -6,25 +6,23 @@ module.exports = {
     node: true,
   },
   extends: [
+    // "eslint:recommended",
     "airbnb",
     "airbnb/hooks",
     // make sure Prettier is last so that it gets a chance to overeride other configs.
-    "prettier",
-    "prettier/react",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: "module",
     jsx: true,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
   },
   plugins: ["react", "prettier"],
   rules: {
     "comma-dangle": [2, "always-multiline"],
-    "prettier/prettier": [
-      2,
-      {
-        trailingComma: "all",
-      },
-    ],
+    "prettier/prettier": [2, { trailingComma: "all" }],
   },
 };
